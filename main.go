746de60946adbe6c7ff7d445ffc80ffa6860bba4
Tijani Eneye Usman <brainyworld10@gmail.com>
2025-12-19 11:30:05 +0100

    wip

--- main.go
+++ main.go
@@ -2,15 +2,71 @@ package main
 
 import (
 	"fmt"
-	"go-expert/oop"
 )
 
+type User struct {
+	Firstname string
+	Lastname  string
+	Age       int
+}
+
+type Payment interface {
+	Initialize()
+	Verify()
+}
+
+type Flutterwave struct {
+	Amount    float64
+	Reference string
+}
+
+type Paystack struct {
+	Amount    float64
+	Reference string
+}
+
+func (f *Flutterwave) Initialize() {
+	fmt.Println("initializing flutterwave")
+}
+
+func (f *Flutterwave) Verify() {
+	fmt.Println("verifying flutterwave")
+}
+
+func (f *Flutterwave) String() string {
+	return fmt.Sprintf("%v", "flutterwave is here")
+}
+
+func (p *Paystack) Initialize() {
+	fmt.Println("initializing paystack")
+}
+
+func (p *Paystack) Verify() {
+	fmt.Println("verifying paystack")
+}
+
+func MakePayment(p Payment) {
+	p.Initialize()
+	p.Verify()
+}
+
+func (u User) FullName() string {
+	return u.Firstname + " " + u.Lastname
+}
+
 func main() {
-	person := &oop.User{
-		Firstname: "John",
-		Lastname:  "Doe",
-		Age:       25,
+	f := Flutterwave{
+		Amount:    10,
+		Reference: "Salary-3282893892",
 	}
 
-	fmt.Println(person.FullName())
+	p := Paystack{
+		Amount:    30,
+		Reference: "Salary-32828924232",
+	}
+
+	fmt.Println(&f)
+
+	MakePayment(&p)
+	MakePayment(&f)
 }
--- oop/main.go
+++ oop/main.go
@@ -1,4 +1,8 @@
-package oop
+package main
+
+import (
+	"fmt"
+)
 
 type User struct {
 	Firstname string
@@ -6,6 +10,63 @@ type User struct {
 	Age       int
 }
 
+type Payment interface {
+	Initialize()
+	Verify()
+}
+
+type Flutterwave struct {
+	Amount    float64
+	Reference string
+}
+
+type Paystack struct {
+	Amount    float64
+	Reference string
+}
+
+func (f *Flutterwave) Initialize() {
+	fmt.Println("initializing flutterwave")
+}
+
+func (f *Flutterwave) Verify() {
+	fmt.Println("verifying flutterwave")
+}
+
+func (f *Flutterwave) String() string {
+	return fmt.Sprintf("%v", "flutterwave is here")
+}
+
+func (p *Paystack) Initialize() {
+	fmt.Println("initializing paystack")
+}
+
+func (p *Paystack) Verify() {
+	fmt.Println("verifying paystack")
+}
+
+func MakePayment(p Payment) {
+	p.Initialize()
+	p.Verify()
+}
+
 func (u User) FullName() string {
 	return u.Firstname + " " + u.Lastname
+}
+
+func check() {
+	f := Flutterwave{
+		Amount:    10,
+		Reference: "Salary-3282893892",
+	}
+
+	p := Paystack{
+		Amount:    30,
+		Reference: "Salary-32828924232",
+	}
+
+	fmt.Println(&f)
+
+	MakePayment(&p)
+	MakePayment(&f)
 }
