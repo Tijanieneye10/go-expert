acc9bd48da103f0f960184ccec79d7cde88ab9b6
Tijani Eneye Usman <brainyworld10@gmail.com>
2025-12-21 20:11:00 +0100

    wip

--- main.go
+++ main.go
@@ -1,39 +1,46 @@
 package main
 
 import (
-	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 )
 
+func DownloadFile(url, path string) error {
+	filename := filepath.Base(path)
+	storePath := filepath.Join(path, filename)
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	output, err := os.Create(storePath)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(output, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func main() {
 
 	url := "https://images.pexels.com/photos/14733025/pexels-photo-14733025.jpeg"
 
-	filePath := filepath.Base(url)
-
-	directory := filepath.Join("./", filePath)
-
-	storeHere, err := os.Create(directory)
+	err := DownloadFile(url, "./download")
 
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-
-	response, err := http.Get(url)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	defer response.Body.Close()
-
-	_, err = io.Copy(storeHere, response.Body)
-
-	if err != nil {
-		return
-	}
-
 }
