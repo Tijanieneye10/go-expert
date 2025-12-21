c2f69bc4e6b8174f045083d177584917e6e71c8e
Tijani Eneye Usman <brainyworld10@gmail.com>
2025-12-21 08:49:32 +0100

    wip

--- concurrency/buffered.go
+++ concurrency/buffered.go
@@ -7,20 +7,15 @@ import (
 
 func main() {
 	message := make(chan string, 3) //Buffered channel
-	done := make(chan bool)
 
 	go func() {
 		fmt.Println("Hello World from main!")
 		message <- "John Doe"
 		message <- "Jane Doe"
-
-		done <- true
 	}()
 
 	time.Sleep(3 * time.Second)
 
 	fmt.Println(<-message)
 	fmt.Println(<-message)
-
-	close(message)
 }
