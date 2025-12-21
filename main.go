e3cf4104eafc63c078300e5e31bea5eae9f43525
Tijani Eneye Usman <brainyworld10@gmail.com>
2025-12-21 06:52:01 +0100

    wip

--- main.go
+++ main.go
@@ -2,55 +2,15 @@ package main
 
 import (
 	"fmt"
-	"go-expert/colorers"
-	"html/template"
-	"os"
-	"strings"
-	"unicode"
+	"time"
 )
 
-type User struct {
-	Firstname string
-	Lastname  string
-	Age       int
+func sayHello() {
+	time.Sleep(2 * time.Second)
+	fmt.Println("Hello World from SayHello!")
 }
 
 func main() {
-	s1 := "John Doe"
-	s2 := strings.Clone(s1)
-
-	s3 := strings.TrimSpace(s2)
-
-	s4 := unicode.IsLetter(rune(s3[0]))
-
-	fmt.Println(s4)
-
-	templates := `
-		Hello, {{ .Firstname }}
-		My age is {{ .Age}}
-	`
-
-	tmp, err := template.New("my_template").Parse(templates)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	err = tmp.Execute(os.Stdout, User{
-		Firstname: "John",
-		Lastname:  "Doe",
-		Age:       42,
-	})
-	if err != nil {
-		return
-	}
-
-	names := []string{"John", "Doe", "foo", "bar"}
-
-	fmt.Println(strings.Join(names, "@"))
-
-	fmt.Println(colorers.Text("Hello John Doe", colorers.Green, colorers.Red))
-
-	value := fmt.Sprintf("%#v", s3)
-	fmt.Println(value)
+	go sayHello()
+	fmt.Println("Hello World from main!")
 }
