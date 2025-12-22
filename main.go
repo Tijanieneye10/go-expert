a0b119091434834009f4b21d2198afe02980e5bd
Tijani Eneye Usman <brainyworld10@gmail.com>
2025-12-22 21:06:36 +0100

    wip

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 func DownloadFile(url, path string) error {
@@ -37,11 +39,31 @@ func DownloadFile(url, path string) error {
 
 func main() {
 
-	url := "https://images.pexels.com/photos/14733025/pexels-photo-14733025.jpeg"
+	//url := "https://images.pexels.com/photos/14733025/pexels-photo-14733025.jpeg"
+	//
+	//err := DownloadFile(url, "./download")
 
-	err := DownloadFile(url, "./download")
-
-	if err != nil {
-		log.Fatal(err)
+	multipleUrl := []string{
+		"https://images.pexels.com/photos/14733025/pexels-photo-14733025.jpeg",
+		"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPygByVCJyLXQ8IXNrEqtUrlwvDXU7Kly3hw&s",
+		"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOlOo3wFz6ukgF8w0vB8LydacAEUhmLcwIcA&s",
 	}
+
+	startTime := time.Now()
+
+	for _, url := range multipleUrl {
+		directoryUpload := "./upload"
+		err := os.MkdirAll(directoryUpload, os.ModePerm)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = DownloadFile(url, directoryUpload)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Println("it took:", time.Since(startTime))
 }
