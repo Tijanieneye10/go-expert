334758d07126d08f9ebab9da7a3f272b45d97145
Tijani Eneye Usman <brainyworld10@gmail.com>
2025-12-31 10:51:37 +0100

    wip

--- main.go
+++ main.go
@@ -2,153 +2,21 @@ package main
 
 import (
 	"database/sql"
-	"embed"
 	_ "embed"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"os"
-	"path/filepath"
-	"sync"
-	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-type Result struct {
-	Url      string
-	Filename string
-	Size     int64
-	Duration time.Duration
-	Error    error
-}
-
-func ConcurrentDownloader(urls []string, destPath string, maxConcurrent int) {
-	if err := os.MkdirAll(filepath.Dir(destPath), 0777); err != nil {
-		log.Fatal(err)
-	}
-
-	results := make(chan Result)
-
-	var wg sync.WaitGroup
-
-	limiter := make(chan struct{}, maxConcurrent)
-
-	for _, url := range urls {
-		wg.Add(1)
-		go func(url string) {
-			defer wg.Done()
-
-			limiter <- struct{}{}
-
-			defer func() { <-limiter }()
-
-			start := time.Now()
-
-			filename := filepath.Base(url)
-			filePath := filepath.Join(destPath, filename)
-
-			storePath, err := os.Create(filePath)
-
-			if err != nil {
-				results <- Result{Url: url, Error: err}
-				log.Fatal(err)
-			}
-
-			resp, err := http.Get(url)
-
-			if err != nil {
-				results <- Result{Url: url, Error: err}
-				_ = os.Remove(filePath)
-				log.Fatal(err)
-			}
-
-			defer resp.Body.Close()
-
-			if resp.StatusCode != http.StatusOK {
-				results <- Result{Url: url, Error: fmt.Errorf("bad status code: %d", resp.StatusCode)}
-			}
-
-			size, err := io.Copy(storePath, resp.Body)
-			if err != nil {
-				results <- Result{Url: url}
-				_ = os.Remove(filePath)
-				log.Fatal(err)
-				return
-			}
-
-			results <- Result{Url: url, Filename: filename, Size: size, Duration: time.Since(start)}
-			fmt.Printf("%s took %s\n", url, time.Since(start))
-
-		}(url)
-	}
-
-	go func() {
-		wg.Wait()
-		close(results)
-	}()
-
-}
-
-func DownloadFile(url, path string, wg *sync.WaitGroup) error {
-
-	defer wg.Done()
-	filename := filepath.Base(url)
-
-	storePath := filepath.Join(path, filename)
-
-	resp, err := http.Get(url)
-
-	if err != nil {
-		_ = os.Remove(storePath)
-		return err
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		_ = os.Remove(storePath)
-	}
-
-	output, err := os.Create(storePath)
-
-	if err != nil {
-		_ = os.Remove(storePath)
-		return err
-	}
-
-	defer output.Close()
-
-	_, err = io.Copy(output, resp.Body)
-	if err != nil {
-		_ = os.Remove(storePath)
-		return err
-	}
-
-	return nil
-}
-
-var counter int = 0
-var mu sync.Mutex
-
-func worker(wg *sync.WaitGroup) {
-	defer wg.Done()
-	mu.Lock()
-	defer mu.Unlock()
-	counter = counter + 1
-}
-
-//go:embed output.txt
-var content string
-
-//go:embed public
-var public embed.FS
-
 func main() {
-	db, err := sql.Open("sqlite3", "./database.db")
+	db, err := sql.Open("sqlite3", "database.sql")
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	createTable(db)
 
 	defer func(db *sql.DB) {
 		err := db.Close()
@@ -163,4 +31,22 @@ func main() {
 	}
 
 	fmt.Println("Connected to database")
+}
+
+func createTable(db *sql.DB) {
+	smtm := `
+	CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+	password VARCHAR(250),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+`
+
+	_, err := db.Exec(smtm)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
