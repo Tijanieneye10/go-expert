809df1f43b68d5c36e8a9c9b04bea112aa269a08
Tijani Eneye Usman <brainyworld10@gmail.com>
2026-01-01 12:26:36 +0100

    wip

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-expert
 
 go 1.25
 
-require github.com/mattn/go-sqlite3 v1.14.32 // indirect
+require (
+	github.com/mattn/go-sqlite3 v1.14.32 // indirect
+	golang.org/x/crypto v0.46.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,9 +5,19 @@ import (
 	_ "embed"
 	"fmt"
 	"log"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
+	"golang.org/x/crypto/bcrypt"
 )
+
+type User struct {
+	Id       int       `json:"id"`
+	Username string    `json:"name"`
+	Email    string    `json:"email"`
+	Password string    `json:"password" db:"_"`
+	Created  time.Time `json:"created"`
+}
 
 func main() {
 	db, err := sql.Open("sqlite3", "database.sql")
@@ -16,7 +26,34 @@ func main() {
 		log.Fatal(err)
 	}
 
-	createTable(db)
+	myUser, err := GetUserByEmail("john@gmail.com", db)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(myUser.Username)
+
+	passwordHashed, err := bcrypt.GenerateFromPassword([]byte("12345678"), bcrypt.DefaultCost)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user := User{
+		Id:       1,
+		Username: "john",
+		Email:    "john2@gmail.com",
+		Password: string(passwordHashed),
+	}
+
+	err = AddUser(&user, db)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//createTable(db)
 
 	defer func(db *sql.DB) {
 		err := db.Close()
@@ -31,6 +68,46 @@ func main() {
 	}
 
 	fmt.Println("Connected to database")
+}
+
+func AddUser(user *User, db *sql.DB) error {
+	stmt, err := db.Prepare("INSERT INTO users(username, email, password) values(?, ?, ?)")
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(user.Username, user.Email, user.Password)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetUserByEmail(email string, db *sql.DB) (*User, error) {
+	query, err := db.Prepare("SELECT * FROM users WHERE email=?")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer query.Close()
+
+	row := query.QueryRow(email)
+
+	var user User
+
+	err = row.Scan(&user.Id, &user.Username, &user.Email, &user.Password, &user.Created)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 func createTable(db *sql.DB) {
