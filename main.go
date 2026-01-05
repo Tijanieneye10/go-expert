23a179df947b062743c59570197b9ddb798b91c2
Tijani Eneye Usman <brainyworld10@gmail.com>
2026-01-05 20:42:00 +0100

    wip

--- main.go
+++ main.go
@@ -1,29 +1,3 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
-func main() {
-	input := bufio.NewScanner(os.Stdin)
-
-	fmt.Print("What is your name? ")
-
-	if input.Scan() {
-		name := input.Text()
-
-		switch name {
-		case "John":
-			output := fmt.Sprintf("How are your? %s", name)
-			fmt.Println(output)
-		case "doe":
-			output := fmt.Sprintf("Good morning %s", name)
-			fmt.Println(output)
-		default:
-			fmt.Println("Invalid name")
-		}
-
-	}
-}
+func main() {}
