bb143447cbc8942afe86aa3fdf48ebd176af3176
Tijani Eneye Usman <brainyworld10@gmail.com>
2026-01-05 20:44:29 +0100

    wip

--- main.go
+++ main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		err := json.NewEncoder(w).Encode(map[string]string{"greet": "hello world"})
+		if err != nil {
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	})
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
